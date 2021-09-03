e7cbae77213fed1de412ae28b969fa9249031648
ashwins78 <ash_sampath@hotmail.com>
2021-09-03 15:08:12 -0700

    The new app create mechanism

--- application.py
+++ application.py
@@ -1,54 +1,87 @@
-#import os
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(application)
+#application = Flask(__name__)
+#application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#db = SQLAlchemy(application)
 
-class Bag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10), unique=True, nullable=False) 
-    use_count = db.Column(db.Integer)
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-    def __repr__(self):
-       return f"{self.id} - {self.name} - {self.use_count}"
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-@application.route('/')
-def index():
-    return 'MyWebserver!'
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-@application.route('/bags')
-def get_bags():
-    result = []
+    @app.route('/')
+    def index():
+        return '!!!MYWEBSERVER!!!'
 
-    bags = Bag.query.all()
-    for bag in bags:
-        bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
-        result.append(bag_data)
+    return app
 
-    return {"bags" : result}
+#class Bag(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(10), unique=True, nullable=False)
+#    use_count = db.Column(db.Integer)
 
-@application.route('/bags/<id>')
-def get_bag(id):
-    bag = Bag.query.get(id)
-    if bag is None:
-        return {"error": "Unknown bag id"}
+#    def __repr__(self):
+#       return f"{self.id} - {self.name} - {self.use_count}"
 
-    bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
-    return {"bag": bag_data}
+#@application.route('/')
+#def index():
+#    if main_called:
+#        return '!!!MYWEBSERVER!!!'
+#    else:
+#        return 'MyWebserver!'
 
-@application.route('/bags/<id>', methods=['PUT'])
-def bag_used(id):
-    bag = Bag.query.get(id)
-    if bag is None:
-        return {"error": "Unknown bag id"}
+#@application.route('/bags')
+#def get_bags():
+#    result = []
 
-    bag.use_count = Bag.use_count + 1
-    db.session.commit()
+#    bags = Bag.query.all()
+#    for bag in bags:
+#        bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
+#        result.append(bag_data)
+
+#    return {"bags" : result}
+
+#@application.route('/bags/<id>')
+#def get_bag(id):
+#    bag = Bag.query.get(id)
+#    if bag is None:
+#        return {"error": "Unknown bag id"}
+
+#    bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
+#    return {"bag": bag_data}
+
+#@application.route('/bags/<id>', methods=['PUT'])
+#def bag_used(id):
+#    bag = Bag.query.get(id)
+#    if bag is None:
+#        return {"error": "Unknown bag id"}
+
+#    bag.use_count = Bag.use_count + 1
+#    db.session.commit()
 
     #bag = Bag.query.get(id)
     #bag_data = {'id': new_bag.id, 'name': new_bag.name, 'use_count': new_bag.use_count}
 
-    bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
-    return {"result": bag_data}
+#    bag_data = {'id': bag.id, 'name': bag.name, 'use_count': bag.use_count}
+#    return {"result": bag_data}
+
+#main_called = False
+#if __name__ == '__main__':
+#    main_called = True
+#    app.run()
